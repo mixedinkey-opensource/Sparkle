b42bf6b7c2d240c746b8605635a4ecaa72b0c1de
Andrew Madsen <andrew@openreelsoftware.com>
2014-06-09 16:38:02 -0600

    Removed redefinition of DEBUG from SUUpdater.h, which broke our use of #ifdef DEBUG in multiple places in our code. Grr...

--- SUUpdater.h
+++ SUUpdater.h
@@ -165,10 +165,6 @@ extern NSString *const SUUpdaterAppcastNotificationKey;
 //	Constants:
 // -----------------------------------------------------------------------------
 
-#ifndef DEBUG
-#define DEBUG	0
-#endif
-
 // Define some minimum intervals to avoid DOS-like checking attacks. These are in seconds.
 #if defined(DEBUG) && DEBUG && 0
 #define SU_MIN_CHECK_INTERVAL 60
